e56b75b74d4a79f2dc255f710acfb8fe44ab6e47
Nightwatch <rdmc0072@yahoo.com>
2021-04-26 17:51:54 -0700

    user can now upload audio files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,27 @@
-
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello')
+    const audioBuffer = new AudioBuffer();
+    audioBuffer.handleAudio();
 }) 
+
+
+class AudioBuffer {
+    constructor() {
+        this.file = document.querySelector(".audio-file");
+        this.audio = document.querySelector(".audio");
+        this.canvas = document.getElementById('canvas');
+    }
+
+    handleCanvas() {
+
+    }
+
+    handleAudio() {
+        this.file.addEventListener('change', (e) => {
+            this.audio.src = URL.createObjectURL(this.file.files[0]);
+            this.audio.load();
+            this.audio.play();
+           
+        })
+    }
+
+}
